59c197cdf10afea6cbb4425d4aed691fdd98161d
Mohammed Ali Shakil <134958706+MOHAMMEDALISHAKIL@users.noreply.github.com>
2023-09-25 13:15:34 +0600

    Create Countelementslinkedlist.c
    Counting the number of elements in a linked list is a common operation when working with linked lists. A linked list is a data structure consisting of nodes, where each node contains a value and a pointer to the next node in the sequence. To count the number of elements in a linked list, we need to traverse the list and increment a counter for each node encountered.

---
+++ Countelementslinkedlist.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+struct Node {
+    int data;
+    struct Node* next;
+};
+int countNodes(struct Node* head) {
+    int count = 0;
+    struct Node* current = head;
+    while (current! = NULL) {
+        count++;
+        current = current->next;
+    }
+    return count;
+}
+int main () {
+    struct Node* head = NULL;
+    struct Node* second = NULL;
+    struct Node* third = NULL;
+    head = (struct Node*) malloc (sizeof(struct Node));
+    second = (struct Node*) malloc (sizeof(struct Node));
+    third = (struct Node*) malloc (sizeof(struct Node));
+    head->data = 1;
+    head->next = second;
+    second->data = 2;
+    second->next = third;
+    third->data = 3;
+    third->next = NULL;
+    printf("Number of nodes in linked list: %d", countNodes(head));
+}
